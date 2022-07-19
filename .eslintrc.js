920b7cbf81c36edb342fff90a1c77c3edb0b6336
Tomoroki <jorgechiva98@gmail.com>
2022-07-19 18:35:12 -0500

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "standard"
+  },
+  "extends": [
+    "standard"
+    "es2021": true
+  },
+  "extends": [
+    "standard"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+	"indent": ["error", 4],
+        "space-before-function-paren": ["error", {
+            anonymous: "always",
+            named: "never",
+            asyncArrow: "always"
+    }]
+  }
+};
