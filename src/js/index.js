c4149e4235270a3aba3ee5861e483d07407fc830
Tomoroki <jorgechiva98@gmail.com>
2022-07-20 21:08:39 -0500

    Avances

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+(function () {
+    const hamburger = document.querySelector('nav__hamburger');
+    const list = document.querySelector('.nav__list');
+    console.log(hamburger);
+    console.log(list);
+    
+    if (hamburger) {
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle("is-active");
+            list.classList.toggle("nav__items--show");
+        });
+    };
+
+})();
