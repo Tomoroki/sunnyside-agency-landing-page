0c40c8a5033c57df97b803bff69533eea600837e
Tomoroki <jorgechiva98@gmail.com>
2022-07-23 22:55:42 -0500

    Fixed eslint and almost completed footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,23 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "extends": [
-    "standard"
-  },
-  "extends": [
-    "standard"
-    "es2021": true
-  },
-  "extends": [
-    "standard"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
-  },
-  "rules": {
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
-	"indent": ["error", 4],
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "standard"
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        indent: ["error", 4],
         "space-before-function-paren": ["error", {
             anonymous: "always",
             named: "never",
             asyncArrow: "always"
-    }]
-  }
+        }]
+    }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,23 @@
 (function () {
-    const hamburger = document.querySelector('.nav__hamburger');
-    const list = document.querySelector('.nav__list');
-    console.log(hamburger);
-    console.log(list);
-    
+    const hamburger = document.querySelector(".nav__hamburger");
+    const list = document.querySelector(".nav__list");
+
     if (hamburger) {
-        hamburger.addEventListener('click', () => {
+        hamburger.addEventListener("click", () => {
             hamburger.classList.toggle("is-active");
             list.classList.toggle("nav__items--show");
         });
-    };
-
+    }
+    // iterar sobre los elementos del nav__list
+    const items = document.querySelectorAll(".nav__list");
+    // le ponemos un if para que solo se ejecute si existe el elemento
+    if (items) {
+        items.forEach(e => {
+            console.log(e);
+            e.addEventListener("click", () => {
+                list.classList.remove("nav__items--show");
+                hamburger.classList.remove("is-active");
+            });
+        });
+    }
 })();
