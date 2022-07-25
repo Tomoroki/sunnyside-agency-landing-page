a184afb018a5b9fb4b85d8bb6dbe82b40d589f20
Tomoroki <jorgechiva98@gmail.com>
2022-07-24 21:06:51 -0500

    Avances desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,50 @@
-// (function () {
-//     const hamburger = document.querySelector(".nav__hamburger");
-//     const list = document.querySelector(".nav__list");
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+document.addEventListener("DOMContentLoaded", function () {
+    scrollNav();
+});
 
-//     // if (hamburger) {
-//     //     hamburger.addEventListener("click", () => {
-//     //         hamburger.classList.toggle("is-active");
-//     //         list.classList.toggle("nav__items--show");
-//     //     });
-//     // }
-//     // iterar sobre los elementos del nav__list
-//     const items = document.querySelectorAll(".nav__list");
-//     // le ponemos un if para que solo se ejecute si existe el elemento
-//     if (items) {
-//         items.forEach(e => {
-//             console.log(e);
-//             e.addEventListener("click", () => {
-//                 list.classList.remove("nav__items--show");
-//                 hamburger.classList.remove("is-active");
-//             });
-//         });
-//     }
-//     document.onclick = function (e) {
-//         if (e.target.classList !== "nav__list" && e.target.classList !== "nav__hamburger") {
-//             list.classList.remove("nav__items--show");
-//             hamburger.classList.remove("is-active");
-//         }
-//     };
-// })();
-
-/** FUNCIONAAAAAAAAAAAAAAAAAA
-    AAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    AAAAAAAAAAAAAAAAAAAAAAAAAAA DE AMBAS FORMAS
- */
 const hamburger = document.querySelector(".nav__hamburger");
 const list = document.querySelector(".nav__list");
-// const hamburger = document.querySelector("#toggle");
-// const list = document.querySelector("#sidebar");
-
-document.onclick = function (e) {
-    if (e.target.classList[0] !== "nav__hamburger" && e.target.classList[0] !== "nav__list") {
-        console.log("click fuera");
-        list.classList.remove("nav__items--show");
-        hamburger.classList.remove("is-active");
-    }
-
-    // FUNCIONA PERFECTAMENTE CON LOS IDS
-    // if (e.target.id !== "toggle" && e.target.id !== "sidebar") {
-    //     console.log("click fuera");
-    //     list.classList.remove("nav__items--show");
-    //     hamburger.classList.remove("is-active");
-    // }
-};
-
-hamburger.onclick = function () {
-    console.log("click");
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("is-active");
     list.classList.toggle("nav__items--show");
-};
+});
+document.addEventListener("click", (e) => {
+    const target = e.target.classList[0];
+    if (target !== "nav__hamburger" && target !== "nav__list") {
+        hamburger.classList.remove("is-active");
+        list.classList.remove("nav__items--show");
+    }
+});
+
+function scrollNav() {
+    const linksNav = document.querySelectorAll(".nav__list li");
+    linksNav.forEach(function (enlace) {
+        enlace.addEventListener("click", scrollSmooth);
+    });
+
+    const linksFooter = document.querySelectorAll(".links");
+    linksFooter.forEach(function (enlace) {
+        enlace.addEventListener("click", scrollSmooth);
+    });
+
+    const btnBack = document.querySelector(".back a");
+    btnBack.addEventListener("click", function (e) {
+        e.preventDefault();
+        const att = document.querySelector(btnBack.attributes.href.value);
+        att.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+}
+
+function scrollSmooth(e) {
+    e.preventDefault();
+    const seccion = document.querySelector(e.target.attributes.href.value);
+    console.log(typeof seccion);
+    seccion.scrollIntoView({
+        behavior: "smooth"
+    });
+}
